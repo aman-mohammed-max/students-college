1f9bb096bdaf6f08ac3fcf87d9ac7ed89cda5e5d
aman-mohammed-max <74004117+aman-mohammed-max@users.noreply.github.com>
2023-04-30 13:39:53 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function App() {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
